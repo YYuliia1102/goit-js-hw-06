a423a8bcfc8027775f62c04f2e7256e9fe15ba9f
YYuliia1102 <reya1102@gmail.com>
2023-04-17 18:38:57 +0300

    deleted coment

--- js/task-02.js
+++ js/task-02.js
@@ -21,15 +21,4 @@ ul.append(...liArray);
 
 console.log(liArray)
 
-// const liArray = []; // створюємо порожній масив
-
-// ingredients.forEach(ingredient => {
-//   const li = document.createElement('li'); // створюємо <li> елемент
-//   li.textContent = ingredient; // додаємо текстовий вміст до <li> елементу
-//   li.classList.add('item'); // додаємо клас item до <li> елементу
-//   liArray.push(li); // додаємо <li> елемент до масиву
-// });
-
-// const ul = document.querySelector('#ingredients'); // знаходимо список ul
-// ul.append(...liArray); // додаємо всі <li> елементи до списку ul
 
